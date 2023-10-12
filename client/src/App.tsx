d6e2dcf80df8a936065e1e93928957de936f1524
Catherine <goldyflakes@gmail.com>
2023-10-11 20:10:23 -0400

    git server & client communicating - kinda

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,24 @@
-import { Switch } from '@headlessui/react'
+import { useEffect, useState } from 'react';
 
 function App() {
+    
+    const [message, setMessage] = useState("");
+    
+    useEffect(() => {
+        const fetchData = async () => {
+            console.log('fetching!');
+            // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+            const msg = await (await fetch('http://localhost:3000/')).json();
+            setMessage(msg);
+        }
+        
+        void fetchData();
+    }, [])
+    
+    
     return (
         <>
-            <h1 className="font-semibold">test</h1>
-            <Switch className="inline-block h-6 w-11 bg-black transition" />
+            <h1 className="font-semibold">{message}</h1>
         </>
     )
 }
